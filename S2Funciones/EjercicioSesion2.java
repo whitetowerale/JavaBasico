cb3d8d0ca79046b92cfdbaa87a3587051e459cc1
Alejandro Muñoz <90469200+whitetowerale@users.noreply.github.com>
2021-10-30 08:58:48 +0200

    Add files via upload

---
+++ S2Funciones/EjercicioSesion2.java
@@ -0,0 +1,22 @@
+package com.S2Funciones;
+
+public class EjercicioSesion2 {
+    /*
+        Para este ejercicio tendreis que crear una función que reciba
+        un precio y devuelva el precio con el IVA incluido
+     */
+
+    private static float PrecioConIva (float precio){
+        float precioConIva = 0.0f;
+        precioConIva = precio * 1.21f;
+        return precioConIva;
+    }
+
+    public static void main(String[] args) {
+        float precio = 15.25f;
+        float precioConIva = 0f;
+        precioConIva = PrecioConIva(precio);
+        System.out.println("El precio con iva de " + precio + " es: " + precioConIva);
+    }
+
+}
